b51188d6800e9f061ed59df2098d1e754c2fd05a
Nobuchika Tanaka <codequokka@gmail.com>
2020-05-29 07:19:39 +0900

    Add tmux settings

--- setup/common/make_links.sh
+++ setup/common/make_links.sh
@@ -7,3 +7,6 @@ ln -snf ${HOME}/dotfiles/bin ${HOME}/.bin
 ln -snf ${HOME}/dotfiles/zshrc ${HOME}/.zshrc
 ln -snf ${HOME}/dotfiles/zsh ${HOME}/.zsh
 ln -snf ${HOME}/dotfiles/p10k.zsh ${HOME}/.p10k.zsh
+
+# Tmux
+ln -snf ${HOME}/dotfiles/tmux.conf ${HOME}/.tmux.conf
