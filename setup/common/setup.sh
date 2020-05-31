9284ae8b149167f0ec0afa1d8c5f053f1ab634db
Nobuchika Tanaka <codequokka@gmail.com>
2020-06-01 07:56:34 +0900

    Add setup scripts

---
+++ setup/common/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+${HOME}/dotfiles/setup/common/install_python3_packages.sh
+
+${HOME}/dotfiles/setup/common/install_zplug.sh
+
+${HOME}/dotfiles/setup/common/make_links.sh
---
+++ setup/ubuntu/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+${HOME}/dotfiles/setup/ubuntu/update_apt_packages.sh
+
+${HOME}/dotfiles/setup/ubuntu/upgrade_apt_packages.sh
+
+${HOME}/dotfiles/setup/ubuntu/install_apt_pakages.sh
